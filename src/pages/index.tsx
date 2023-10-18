841b09df0c6bdbbb93c34eb5afb00075a5173d42
mac <macjavellana@gmail.com>
2023-10-18 19:42:43 +0800

    bug fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -29,7 +29,7 @@ export default function Home() {
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
         <div>
-          Hello, {userId} your current active session is {sessionId}
+          Hello, {user?.username} your current active session is {sessionId}
         </div>
         <UserButton afterSignOutUrl="/"/>
       </main>
